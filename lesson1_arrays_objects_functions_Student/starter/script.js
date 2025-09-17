41bb8fe90fdaf71ce4fb6a48f6eb82a87878e171
Jackapong Karapinvong <129889438+Lucky265225@users.noreply.github.com>
2025-09-17 12:33:53 +0700

    week06

---
+++ lesson1_arrays_objects_functions_Student/starter/script.js
@@ -0,0 +1,71 @@
+// Lesson 1: Arrays, Objects, and Functions
+
+// 1. Create an empty array to hold the quotes
+const quotes = []
+
+/*
+  2. Function: addQuote
+  - Accepts a quote object with id, content, and author
+  - Adds it to the quotes array
+*/
+function addQuote(quote) {
+  // TODO: Add the quote object to the quotes array
+  quotes.push(quote)
+}
+
+/*
+  3. Function: deleteQuote
+  - Accepts an id
+  - Removes the quote with that id from the array
+*/
+function deleteQuote(id) {
+  // TODO: Remove the quote object from the array using the given id
+  quotes.splice(quotes.findIndex( q => q.id === id),1)
+}
+
+/*
+  4. Function: updateQuote
+  - Accepts an id and an object with new content and/or author
+  - Updates the quote with the given id
+*/
+function updateQuote(id, updatedQuote) {
+  // TODO: Find the quote by id and update its properties
+  let newQuote = quotes.findIndex( q => q.id === id)
+  let {content,author} = updatedQuote
+  if(content !== undefined){
+    quotes[newQuote].content = content 
+  }
+  if(author !== undefined){
+    quotes[newQuote].author = author
+  }
+}
+
+/*
+  5. Function: getAllQuotes
+  - Returns all quotes in the array
+*/
+function getAllQuotes() {
+  // TODO: Return the quotes array
+  return quotes
+}
+
+// 6. Test your functions below
+// TODO: Add 3 quotes using addQuote()
+
+// TODO: Delete 1 quote using deleteQuote()
+
+// TODO: Update 1 quote using updateQuote()
+
+// TODO: Print all quotes using getAllQuotes()
+
+addQuote({ id: 1, content: "The only limit to our realization of tomorrow is our doubts of today.", author: "Franklin D. Roosevelt" })
+addQuote({ id: 2, content: "In the middle of every difficulty lies opportunity.", author: "Albert Einstein" })
+addQuote({ id: 3, content: "It does not matter how slowly you go as long as you do not stop.", author: "Confucius" })
+
+deleteQuote(2)
+
+updateQuote(1,{ id: 1, content: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" })
+
+console.log(getAllQuotes());
+
+
---
+++ week06/functionProgramming.js
@@ -0,0 +1,59 @@
+const math = (numbers,formatter) => formatter(numbers)
+const add = num => num.reduce((x,y) => x + y)
+const multiple = num => num.reduce((x,y) => x * y)
+
+// console.log(math([1,2,3,4],add))
+// console.log(math([1,2,3,4],multiple))
+
+function a() {
+  return sum
+}
+function b() {
+  return sum(1, 5)
+}
+function c(x) {
+  return x
+}
+function sum(n1, n2) {
+  return n1 + n2
+}
+
+// console.log(typeof a())
+// console.log(typeof b())
+// console.log(typeof c(sum))
+
+function addSquares(a, b) { 
+    function square(x) { 
+        return x * x
+    } 
+    return square(a) + square(b)
+} 
+// console.log(addSquares(3,3));
+
+// let aa = 1 //global scope
+// function doSomething(x) {
+//   let aa = 10 //local scope
+//   let greet = "Hello"
+//   function echo() {
+//     let b = "Bob"
+//     console.log(aa, b)
+//   }
+//   echo()
+//   return `${greet}, ${x}, a=${aa}`
+// }
+// console.log(doSomething("guest"))
+// aa = 100
+// console.log(`a= ${aa}`)
+
+
+function doSomething(x) {
+  function echo() {
+    return `hello,${x}`
+  }
+  return echo
+}
+// console.log(doSomething("guest")())
+
+
+
+ 
